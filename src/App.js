1cf49823dd1817c9b8f9aa1b74e085d069a793c6
MattScarthSaunders <m.scarsaund@gmail.com>
2022-12-05 11:49:16 +0000

    installing axios and router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        <main></main>
+      </header>
+    </div>
+  );
 }
 
 export default App;
