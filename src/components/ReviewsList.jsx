e53cbf0f03597bb12612791ed0da1431b4372ef4
MattScarthSaunders <m.scarsaund@gmail.com>
2022-12-05 12:46:22 +0000

    added loading message

--- src/components/ReviewsList.jsx
+++ src/components/ReviewsList.jsx
@@ -1,47 +1,49 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getReviews } from "./api";
 import { ReviewsContext } from "./contexts/ReviewContext";
 
 const ReviewsList = () => {
   const { reviews, setReviews } = useContext(ReviewsContext);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     getReviews().then((receivedReviews) => {
       setReviews(receivedReviews);
     });
+    setIsLoading(false);
   }, []);
 
-  return (
-    <>
-      <ul className="Reviews--List">
-        {reviews.map(
-          ({
-            review_id,
-            title,
-            owner,
-            review_img_url,
-            created_at,
-            votes,
-            comment_count,
-          }) => {
-            return (
-              <li key={review_id} className="Reviews--List--Card">
-                <h3>{title}</h3>
-                <img src={review_img_url} alt={title} />
-                <p>User: {owner}</p>
-                <p>Votes: {votes}</p>
-                <p>Comments: {comment_count}</p>
-                <p>Posted: {created_at.slice(0, 10)}</p>
-                <Link to={`/reviews/${review_id}`}>
-                  <button>See Review</button>
-                </Link>
-              </li>
-            );
-          }
-        )}
-      </ul>
-    </>
+  return isLoading ? (
+    <p>loading...</p>
+  ) : (
+    <ul className="Reviews--List">
+      {reviews.map(
+        ({
+          review_id,
+          title,
+          owner,
+          review_img_url,
+          created_at,
+          votes,
+          comment_count,
+        }) => {
+          return (
+            <li key={review_id} className="Reviews--List--Card">
+              <h3>{title}</h3>
+              <img src={review_img_url} alt={title} />
+              <p>User: {owner}</p>
+              <p>Votes: {votes}</p>
+              <p>Comments: {comment_count}</p>
+              <p>Posted: {created_at.slice(0, 10)}</p>
+              <Link to={`/reviews/${review_id}`}>
+                <button>See Review</button>
+              </Link>
+            </li>
+          );
+        }
+      )}
+    </ul>
   );
 };
 
